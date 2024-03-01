27651fbc1e7712932ea78c499465ec12b9c856c6
Ivan Rimola <rimola7@gmail.com>
2024-03-01 15:35:49 +0100

    draft: card in detail

--- src/components/poke-detail.tsx
+++ src/components/poke-detail.tsx
@@ -1,5 +1,3 @@
-import {Outlet, useParams} from "react-router-dom";
-
 export default function PokeDetail(data: any) {
   
   console.log("Poke detail data: ", data)
@@ -13,7 +11,6 @@ export default function PokeDetail(data: any) {
           <span>Abilities: </span>
           {data.data?.abilities.map((abilitiy, index) => (
             <span>
-              
               {index === 0 ? abilitiy.ability.name : ', ' + abilitiy.ability.name}
             </span>
           ))}
@@ -21,26 +18,26 @@ export default function PokeDetail(data: any) {
         <img src={data.data?.sprites.front_default}/>
       </div>
       
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      {/*<Card sx={{ maxWidth: 345 }}>*/}
+      {/*  <CardMedia*/}
+      {/*    sx={{ height: 140 }}*/}
+      {/*    image="/static/images/cards/contemplative-reptile.jpg"*/}
+      {/*    title="green iguana"*/}
+      {/*  />*/}
+      {/*  <CardContent>*/}
+      {/*    <Typography gutterBottom variant="h5" component="div">*/}
+      {/*      Lizard*/}
+      {/*    </Typography>*/}
+      {/*    <Typography variant="body2" color="text.secondary">*/}
+      {/*      Lizards are a widespread group of squamate reptiles, with over 6,000*/}
+      {/*      species, ranging across all continents except Antarctica*/}
+      {/*    </Typography>*/}
+      {/*  </CardContent>*/}
+      {/*  <CardActions>*/}
+      {/*    <Button size="small">Share</Button>*/}
+      {/*    <Button size="small">Learn More</Button>*/}
+      {/*  </CardActions>*/}
+      {/*</Card>*/}
     </>
   )
 }
